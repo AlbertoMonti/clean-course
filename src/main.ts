e0fd804bba9e505f2050e9d4f540881dd31a27e0
Alberto <monta@LAPTOP-QH1UOLAN>
2023-02-01 19:28:37 +0100

    Prueba credenciales

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import './style.css'
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 
+// Cambio prueba
+
 app.innerHTML = `
   <h1>CleanCode y SOLID</h1>
   <span>Revisar la consola de JavaScript</span>
