2ea46788b035400214cbc0e797034e0f69e6a1fb
Alberto <monta@LAPTOP-QH1UOLAN>
2023-02-01 19:29:46 +0100

    Correccion prueba credenciales

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import './style.css'
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 
-// Cambio prueba
-
 app.innerHTML = `
   <h1>CleanCode y SOLID</h1>
   <span>Revisar la consola de JavaScript</span>
